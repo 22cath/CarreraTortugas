bb8bfdf9d1d17649523e5353d10f273125bff406
22cath <cecollignon@gmail.com>
2022-02-20 16:00:39 +0100

    Definición del método COMPETIR, con avance random, establecimiento tortuga ganadora al cruzar linea finish, mensaje de tortuga ganadora y break

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 class Circuito():
     corredores = []
@@ -12,9 +13,9 @@ class Circuito():
         self.__startLine = (-width / 2) + 20 # en lugar de 20, un 5% de ancho total?
         self.__finishLine = (width / 2) - 20
         
-  #      self.__createRunners()
+        self.__createRunners()
     
-  #  def createRunners(self):         
+    def __createRunners(self):         
         for i in range(4): # lo hacemos en un bucle
             new_turtle = turtle.Turtle()
             new_turtle.color(self.__colorTurtle[i]) 
@@ -23,8 +24,22 @@ class Circuito():
             new_turtle.setpos(self.__startLine, self.__posStartY[i])
                                    
             self.corredores.append(new_turtle)
-              
-              
-
+    
+   # avanzando por turno, como tirando dados, en cada turno, cada instancia hace una accion.
+    def competir(self):
+          
+        hayGanador = False
+          
+        while not hayGanador:
+            for tortuga in self.corredores:
+                avance = random.randint(1, 10)
+                tortuga.forward(avance)
+                  
+                if tortuga.position()[0] >= self.__finishLine:
+                    hayGanador = True
+                    print('La tortuga de color {} ha ganado'.format(tortuga.color()[0]))
+                    break 
+                
 if __name__ == '__main__':
     circuito = Circuito(640, 480)
+    circuito.competir()
